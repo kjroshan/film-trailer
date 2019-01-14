c19a7465a18b9a2a0930b850fa2144e65f68e7f3
JAMES, ROSHAN <kjroshan@gmail.com>
2019-01-14 14:53:16 -0500

    redis connection issue fixed

--- src/redis.js
+++ src/redis.js
@@ -9,7 +9,6 @@ const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
 const REDIS_PORT = process.env.REDIS_PORT || '6379';
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD || '';
 
-let redisIsConnected = false;
 
 function initializeRedis(logger) {
     try {
@@ -17,15 +16,12 @@ function initializeRedis(logger) {
             host: REDIS_HOST,
             port: REDIS_PORT,
             password: REDIS_PASSWORD,
-            db: 'TrailerApplicationCache',
-            tls: { rejectUnauthorized: false },
             retry_strategy(options) {
                 if (options.error && options.error.code === 'ECONNREFUSED') {
                     logger.log('error', 'There seems to be issue with redis connection', options.error);
-                    // return new Error('The server refused the connection', options.error);
                 }
                 if (options.total_retry_time > 1000 * 60 * 60) {
-                    // return new Error('Retry time exhausted', options.error);
+                    logger.log('error', 'Retry time exhausted', options.error);
                 }
                 if (options.attempt > 10) {
                     return undefined;
@@ -35,12 +31,10 @@ function initializeRedis(logger) {
         });
 
         client.on('connect', () => {
-            redisIsConnected = true;
             logger.log('info', 'Redis client connected');
         });
 
         client.on('error', (err) => {
-            redisIsConnected = false;
             logger.log('error', 'Error connecting to redis', err);
         });
 
@@ -57,14 +51,13 @@ function redisSwitcher(logger) {
     this.devCache = createDevCache(logger);
 
     this.getAsync = (...args) => {
-        if (redisIsConnected) {
-            logger.log('info', 'Redis Connection is up');
+        if (this.client.connected) {
             return this.client.getAsync(...args);
         }
         return this.devCache.getAsync(...args);
     };
     this.setAsync = (...args) => {
-        if (redisIsConnected) {
+        if (this.client.connected) {
             return this.client.setAsync(...args);
         }
         return this.devCache.setAsync(...args);
